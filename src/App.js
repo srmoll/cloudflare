8cab5d0e9e694e3a0981f65c69b5642679385909
Sarah Moll <srmoll@umich.edu>
2021-10-20 17:28:02 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+import { Router } from "@reach/router";
+
+import Posts from './components/posts'
+import Post from './components/post'
+
+function App() {
+  return (
+    <Router>
+      <Posts path="/" />
+      <Post path="/posts/:id" />
+    </Router>
+  );
+}
+
+export default App;
